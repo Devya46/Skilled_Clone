e90f53eb152d9a35759d81ef5b886314a066a7c5
Devya46 <deviyani097@gmail.com>
2026-01-17 12:23:28 +0530

    final copy

--- Clone/script.js
+++ Clone/script.js
@@ -5,3 +5,22 @@ function openDetails() {
 function closeDetails() {
   document.getElementById("coursePopup").style.display = "none";
 }
+// ------------------
+
+const openBtn = document.getElementById("openModal");
+const modal = document.getElementById("instructorModal");
+const closeBtn = document.querySelector(".close");
+
+openBtn.onclick = () => {
+  modal.style.display = "flex";
+};
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
